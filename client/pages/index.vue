4ee31e21696b93ae955756ac0186618ef3999a34
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2022-12-11 23:28:10 +0100

    General layout

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-
+  <div class="row">
+    <div class="col-12">
+      <PageTitle page_title="Starters"/>
+      <p>
+        ....
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
