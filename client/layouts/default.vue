9b21d7b7e2624299a325918f98e831d3e3327061
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2022-12-11 23:08:38 +0100

    General layout - work in progress

---
+++ client/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <div class="p-3">
+            <Logo/>
+            <TopMenu/>
+            <Nuxt/>
+        </div>
+    </div>
+</template>
+
+<script>
+import TopMenu from '../components/TopMenu.vue'
+
+export default {
+  components: { TopMenu },
+    layout: 'homepage',
+}
+</script>
