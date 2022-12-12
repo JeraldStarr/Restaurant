eb18219f1af59fee8e9ac7d7ffe11bec89215a97
Lukasz Sitnik <lukasz.sitnik@gmail.com>
2022-12-12 23:25:51 +0100

    Added bootstrap-icon package and add bag icon to top menu

--- client/components/TopMenu.vue
+++ client/components/TopMenu.vue
@@ -3,7 +3,7 @@
     <nuxt-link to="/">Starters</nuxt-link>
     <nuxt-link to="/lunch">Lunch</nuxt-link>
     <nuxt-link to="/desserts">Desserts</nuxt-link>
-    <nuxt-link to="/yourorder">Your Order (0)</nuxt-link>
+    <nuxt-link to="/yourorder">Your Order <i class="bi bi-bag"></i> (0)</nuxt-link>
   </div>
 </template>
 
